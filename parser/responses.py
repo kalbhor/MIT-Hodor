59a9107972656b8d15e3728a0d469063bdc037de
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-08-24 12:11:48 +0530

    Improved initial message

--- parser/responses.py
+++ parser/responses.py
@@ -15,4 +15,16 @@ class messages:
 
     TOS = "Terms of service : "
 
-    menu = "Hey, I am MIT Hodor, a messenger bot.\n\n I can tell you about your:\n\n- Attendance\n- Timetable\n- Teacher guardian\n- Whether you can bunk a class or not\n\n NOTE : I am currently under construction, so you might get double replies or weird information (along with relevant info) \n\n"
+    menu = """
+    Hey, I am MIT Hodor, a messenger bot.\n\n
+    I can tell you about your:\n\n
+    - Attendance\n
+    - Timetable\n
+    - Teacher guardian\n
+    - Whether you can bunk a class or not\n\n
+    I was built by\n
+    - Lakshay Kalbhor : fb.com/KalbhorLakshay\n
+    - Neel Vashisht : fb.com/profile.neel\n
+    - Ananya Kaushik: fb.com/ananya.kaushik.75\n\n
+    NOTE : I am currently under construction, so you might sometime get double replies, etc.\n\n
+    """

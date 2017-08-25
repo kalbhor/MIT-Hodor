1d2dbfe71bdb7a6055f6ee9b162de6e2a5dcf199
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-08-26 01:15:17 +0530

    Changed order of quick menu

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ dbase = database.handler(db)
 responder = responses.messages()
 
 quick_replies = [
+        fbmq.QuickReply(title="What can you do?", payload="WHAT"),
         fbmq.QuickReply(title="Attendance", payload="ATTENDANCE"),
         fbmq.QuickReply(title="Timetable", payload="TIMETABLE"),
         fbmq.QuickReply(title="Teacher Guardian", payload="TEACHER"),
-        fbmq.QuickReply(title="What can you do?", payload="WHAT"),
     ]
 
 ### DB Skeleton ###
